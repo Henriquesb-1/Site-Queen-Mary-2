5bdde2f8c211117674496ffcf6fcb9160dce8760
Henriquesb-1 <104944100+Henriquesb-1@users.noreply.github.com>
2022-11-10 21:35:55 -0300

    Add files via upload

---
+++ scripts/atracao.js
@@ -0,0 +1,4 @@
+const slideControle = document.getElementById("slide2")
+if(slideControle.checked) {
+    document.querySelector(".slideContent").style.position.left = "-100%"
+}
---
+++ scripts/slideBack/slide.js
@@ -0,0 +1,4 @@
+let images = ['<img src="qm2back2.jpg">']
+document.body.addEventListener('click', () => {
+    document.body.style.background = images[0]
+})
